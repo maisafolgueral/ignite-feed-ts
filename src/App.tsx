5fb79cd7c47a620731b653dd7ee3058b71517666
maisafolgueral <maisa.folgueral@gmail.com>
2024-05-04 12:41:27 -0300

    Added extend interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 // JSX = JavaScript + XML (HTML)
-import { Post } from "./components/Post";
+import { Post, PostType } from "./components/Post";
 import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
 
 import "./global.css";
 import styles from "./App.module.css";
 
-const posts = [
+const posts: PostType[] = [
   {
     id: 1,
     author: {
@@ -75,9 +75,7 @@ export function App() {
             return (
               <Post
                 key={post.id}
-                author={post.author}
-                content={post.content}
-                publishedAt={post.publishedAt}
+                post={post}
               />
             );
           })}
